5590eea54b644f80f2d7a735a001628ebba304a7
EmptinessBoy <i@my.huxiaofan.com>
2020-11-20 11:25:27 +0800

    - 修复了 utf8 时区不同步的问题

--- src/com/huxiaofan/api/AddMembers.java
+++ src/com/huxiaofan/api/AddMembers.java
@@ -13,11 +13,73 @@ import java.io.Writer;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
 @WebServlet(name = "AddMembers")
 public class AddMembers extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("application/json; charset=utf-8");
+        //允许跨域请求
+        response.setHeader("Access-Control-Allow-Origin", "*"); //  这里最好明确的写允许的域名
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8081"); //  这里最好明确的写允许的域名
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type,Access-Token,Authorization,ybg");
+
+        //定义输出对象
+        Writer o = response.getWriter();
+        //result接口
+        ResultSet rs;
+        //新的数据工具类对象
+        dbUtils db = new dbUtils();
+        //创建stmt类
+        Statement stmt = db.getStatement();
+
+        String cno = request.getParameter("id");
+        String cname = request.getParameter("name");
+        String csex = request.getParameter("sex");
+        String cmoney = request.getParameter("money");
+        String caddress = request.getParameter("address");
+        String cregtime = request.getParameter("date").substring(0,19);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        Date d = null;
+        try {
+            d = sdf.parse(cregtime);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        //这句话加8小时
+        d.setTime(d.getTime() + 8 * 60 * 60 * 1000);
+        //System.out.println(d);
+        //时间对象转字符串
+        cregtime = sdf.format(d);
+        System.out.println(cno+cname+csex+cmoney+caddress+cregtime);
+
+
+//
+//
+//        String d = "delete from members where cno = \"" + cno + "\"";
+//        try {
+//            if (stmt.executeUpdate(d) == 0){
+//                o.write("Fail，删除失败！");
+//                System.out.println("Fail，删除失败！" + d);
+//                response.setStatus(202);
+//            }
+//            System.out.println("OK，删除成功！" + cno);
+//            System.out.println(d);
+//            o.write("OK，删除成功！");
+//        } catch (SQLException throwables) {
+//            o.write("Fail，删除失败！");
+//            System.out.println("Fail，删除失败！" + d);
+//            throwables.printStackTrace();
+//            response.setStatus(204);
+//        }
+
 
     }
 
--- src/com/huxiaofan/api/ListMembers.java
+++ src/com/huxiaofan/api/ListMembers.java
@@ -1,5 +1,6 @@
 package com.huxiaofan.api;
 //此类实现用户列表查询，用户信息更新和用户删除
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +11,11 @@ import java.io.Writer;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+
 import com.alibaba.fastjson.*;
 
 @WebServlet(name = "ListMembers")
@@ -40,7 +45,7 @@ public class ListMembers extends HttpServlet {
             String cno = request.getParameter("cno");
             String d = "delete from members where cno = \"" + cno + "\"";
             try {
-                if (stmt.executeUpdate(d) == 0){
+                if (stmt.executeUpdate(d) == 0) {
                     o.write("Fail，删除失败！");
                     System.out.println("Fail，删除失败！" + d);
                     response.setStatus(202);
@@ -54,33 +59,44 @@ public class ListMembers extends HttpServlet {
                 throwables.printStackTrace();
                 response.setStatus(204);
             }
-        }else if (request.getParameter("method").equals("modify")) {
+        } else if (request.getParameter("method").equals("modify")) {
             //从参数获取前端需要修改的学生ID
             String cno = request.getParameter("row[cno]");
             String cname = request.getParameter("row[cname]");
             String caddress = request.getParameter("row[caddress]");
             //因为前端来的数据形如 cregtime="1986-01-07T16:00:00.000Z"需要对其进行处理
-            String cregtime = request.getParameter("row[cregtime]").substring(0,16);
+            String cregtime = request.getParameter("row[cregtime]").substring(0, 19);
             String csex = request.getParameter("row[csex]");
 
-            System.out.println(cno+" "+cname+" "+caddress+" "+cregtime+" "+csex);
-            //先做查询看是否存在
-            String m = "update members set cname=\"" + cname + "\", csex=\"" + csex + "\", caddress=\"" + caddress + "\", cregtime=\"" + cregtime + "\" where cno=\"" + cno + "\"";
+            System.out.println("取得的参数为：" + cno + " " + cname + " " + caddress + " " + cregtime + " " + csex);
+            //用于sql出错分析
+            // String m = "update members set cname=\"" + cname + "\", csex=\"" + csex + "\", caddress=\"" + caddress + "\", cregtime=\"" + cregtime + "\" where cno=\"" + cno + "\"";
+
             try {
+                //先转换为时间对象是为了解决时区UTF-8同步的的问题
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+                Date d = sdf.parse(cregtime);
+                //这句话加8小时
+                d.setTime(d.getTime() + 8 * 60 * 60 * 1000);
+                //System.out.println(d);
+                //时间对象转字符串
+                cregtime = sdf.format(d);
+                //sql语句拼接
+                String m = "update members set cname=\"" + cname + "\", csex=\"" + csex + "\", caddress=\"" + caddress + "\", cregtime=\"" + cregtime + "\" where cno=\"" + cno + "\"";
                 System.out.println(m);
+                //打印行数
                 int count = stmt.executeUpdate(m);
                 if (count > 0) {
                     o.write("修改成功，影响行数：" + count);
                 }
                 System.out.println("修改成功，影响行数：" + count);
-            } catch (SQLException throwables) {
+            } catch (SQLException | ParseException throwables) {
                 o.write("Fail，修改失败！");
-                System.out.println("Fail，修改失败！" + m);
+                System.out.println("Fail，修改失败！");
                 response.setStatus(204);
                 throwables.printStackTrace();
             }
         }
-
 
         //使用定义的工具类一键断开con和stmt连接
         db.closeConnect();
