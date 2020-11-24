84c54343f18687aea8c748dd9b5f538dd861cd77
EmptinessBoy <i@my.huxiaofan.com>
2020-11-24 13:05:53 +0800

    - 增加注销 api

--- src/com/huxiaofan/api/LogoutApi.java
+++ src/com/huxiaofan/api/LogoutApi.java
@@ -6,11 +6,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 @WebServlet(name = "LogoutApi")
 public class LogoutApi extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
+
+        //封装的http请求响应头
+        httpUtils.httpUtil(request, response);
+
+        //定义输出对象
+        Writer o = response.getWriter();
+
+        String token = request.getParameter("token");
+        System.out.println("等待注销的 token 为：" + token);
+
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
