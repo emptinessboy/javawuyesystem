78eed29df4727916e65c850856853dda1741010d
EmptinessBoy <i@my.huxiaofan.com>
2020-11-26 14:23:46 +0800

    - 接单功能开发完毕

--- src/com/huxiaofan/api/OrderManage.java
+++ src/com/huxiaofan/api/OrderManage.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.Writer;
 import java.sql.ResultSet;
@@ -17,7 +18,82 @@ import java.util.HashMap;
 @WebServlet(name = "OrderManage")
 public class OrderManage extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //此方法用来接受和结束工单
+        //封装的http请求响应头
+        httpUtils.httpUtil(request, response);
+        //定义输出对象
+        Writer o = response.getWriter();
 
+        String token = request.getParameter("token");
+        String method = request.getParameter("method");
+
+
+        if (method.equals("accept")) {
+
+            HttpSession hs = (HttpSession) getServletContext().getAttribute(token);
+            String staff = (String) hs.getAttribute("eid");
+            String id = request.getParameter("id");
+
+            String ac = "update orders set status=\"" + "2" + "\", staff=\"" + staff + "\" where id=\"" + id + "\"";
+
+            System.out.println(ac);
+            //新的数据工具类对象
+            dbUtils db = new dbUtils();
+            //创建stmt类
+            Statement stmt = db.getStatement();
+            try {
+                if (stmt.executeUpdate(ac) == 0) {
+                    o.write("Fail，接单失败！");
+                    System.out.println("Fail，接单失败！");
+                    response.setStatus(202);
+                } else {
+                    response.setStatus(200);
+                    System.out.println("OK，接单成功！");
+                    System.out.println(ac);
+                    o.write("OK，接单成功！");
+                }
+            } catch (SQLException throwables) {
+                o.write("Fail，接单失败！");
+                System.out.println("Fail，接单失败！");
+                throwables.printStackTrace();
+                response.setStatus(204);
+            } finally {
+                System.out.println("接单物业服务成功");
+                //使用定义的工具类一键断开con和stmt连接
+                db.closeConnect();
+            }
+        } else {
+//            //新的数据工具类对象
+//            dbUtils db = new dbUtils();
+//            //创建stmt类
+//            Statement stmt = db.getStatement();
+//            //反之则添加服务
+//            String s = "INSERT INTO service" +
+//                    "(sid,sname,sprice,sdesc,stime)" +
+//                    "VALUES" +
+//                    "(\'" + sid + "\',\'" + sname + "\',\'" + sprice + "\',\'" + sdesc + "\',\'" + stime + "\')";
+//            try {
+//                if (stmt.executeUpdate(s) == 0) {
+//                    o.write("Fail，插入失败！");
+//                    System.out.println("Fail，插入用户信息失败！" + s);
+//                    response.setStatus(202);
+//                } else {
+//                    response.setStatus(200);
+//                    System.out.println("OK，新增成功！" + sid);
+//                    System.out.println(s);
+//                    o.write("OK，新增成功！");
+//                }
+//            } catch (SQLException throwables) {
+//                o.write("Fail，插入失败！");
+//                System.out.println("Fail，插入失败！" + s);
+//                throwables.printStackTrace();
+//                response.setStatus(204);
+//            }finally {
+//                System.out.println("新增物业服务成功");
+//                //使用定义的工具类一键断开con和stmt连接
+//                db.closeConnect();
+//            }
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
